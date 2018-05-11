099542c94859ad0c3281eaff03ceb3978cf1afd9
Adiger Nikolay <naim83854@gmail.com>
2018-05-11 15:35:33 +0300

    Version 1

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://Admin:123321@ds247058.mlab.com:47058/library';
-// const port = 8080;
+const url = 'mongodb://Admin:123321@ds119110.mlab.com:19110/users_crud';
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -20,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 module.exports = MongoClient.connect(url, (err, client) => {
     if (err) return console.log(err);
-    db = client.db('library');
+    db = client.db('users_crud');
     app.listen(port, () => {
         console.log('listening on:', port);
     })
@@ -32,39 +31,18 @@ app.use('*', function (req, res, next) {
 });
 
 app.get('/', (req, res) => {
-        var cursor = db.collection('quotes').find()
+        var cursor = db.collection('quotes').find();
     }
 );
 
-app.get('/count', (req, res) => {
-    db.collection('count').find().toArray((err, result) => {
+app.get('/users', (req, res) => {
+    db.collection('users').find().toArray((err, result) => {
         if (err) return console.log(err);
-        res.send(result[0]);
-    })
-});
-
-app.put('/count/:id', (req, res) => {
-    const id = req.params.id;
-    const details = {'_id': new ObjectID(id)};
-    const count = {$set: {counts: req.body.counts}};
-    db.collection('notes').updateOne(details, count, (err, result) => {
-        if (err) {
-            res.send({'error': 'An error has occurred'});
-        } else {
-            res.send(count);
-        }
-    });
-});
-
-app.get('/books', (req, res) => {
-    db.collection('books').find().toArray((err, result) => {
-        if (err) return console.log(err);
-
         res.send(result);
     })
 });
 
-app.post('/books', function (req, res) {
+app.post('/users', function (req, res) {
 
     var post = [];
     req.on('data', chuck => post.push(chuck)
@@ -74,7 +52,7 @@ app.post('/books', function (req, res) {
 
         var body = JSON.parse(Buffer.concat(post));
 
-        db.collection('books').insertOne(body, (err, result) => {
+        db.collection('users').insertOne(body, (err, result) => {
             if (err) {
                 res.send({'error': 'An error has occurred'});
             } else {
@@ -84,11 +62,11 @@ app.post('/books', function (req, res) {
     })
 });
 
-app.delete('/books/:id', (req, res) => {
+app.delete('/users/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
 
-    db.collection('books').deleteOne(details, (err, result) => {
+    db.collection('users').deleteOne(details, (err, result) => {
         if (err) {
             res.send({'error': 'An error has occurred'});
         } else {
@@ -97,7 +75,7 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-app.put('/books/:id', (req, res) => {
+app.put('/users/:id', (req, res) => {
 
     var post = [];
     req.on('data', chuck => post.push(chuck)
@@ -109,12 +87,11 @@ app.put('/books/:id', (req, res) => {
         var myquery = {_id: new ObjectID(body._id)};
         var newvalues = {
             $set: {
-                author: body.author,
-                tittle: body.tittle,
-                date: body.date
+                name: body.name,
+                role: body.role
             }
         };
-        db.collection('books').findOneAndUpdate(myquery, newvalues, (err, result) => {
+        db.collection('users').findOneAndUpdate(myquery, newvalues, (err, result) => {
             if (err) {
                 res.send({'error': 'An error has occurred'});
             } else {
